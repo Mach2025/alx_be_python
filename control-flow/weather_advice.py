47779dd3e1be81f9e6a63f935c90e146187841ea
Rebecca Machio <ajiamborebecca26@gmail.com>
2025-05-31 12:25:52 +0300

    Changes made to the file

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -6,4 +6,4 @@ elif Weather == "rainy":
 elif Weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
-    print("Sorry, I don't have recommendations for this weather.")
+    print(" Sorry, I don't have recommendations for this weather")
