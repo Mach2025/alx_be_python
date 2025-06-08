008c3205202bedee2aa487a1ef78b426a0d93864
Rebecca Machio <ajiamborebecca26@gmail.com>
2025-06-08 17:41:18 +0300

    Arithmetic Operations Function

---
+++ control-flow/Alx_practise/calculator.py
@@ -0,0 +1,14 @@
+# Creating and Using Modules
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply (x, y):
+    return x * y
+
+def divide(x, y):    
+    return x / y
+
+
